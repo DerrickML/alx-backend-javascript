557ad781fb540aa77aefd93f953fef436ecb2878
DeroMal <derrickmla123@gmail.com>
2023-01-04 02:45:45 +0300

    03

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,19 @@
+export default function getListStudents () {
+  return new Array(
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Francisco'
+    },
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia'
+    },
+    {
+      id: 5,
+      firstname: 'Serena',
+      location: 'San Francisco'
+    }
+  );
+}
---
+++ 0x03-ES6_data_manipulation/2-main.js
@@ -0,0 +1,7 @@
+import getListStudents from './0-get_list_students.js';
+import getStudentsByLocation from './2-get_students_by_loc.js';
+
+const students = getListStudents();
+
+console.log(students.filter((e) => e.id === 2));
+console.log(getStudentsByLocation(students, 'San Francisco'));

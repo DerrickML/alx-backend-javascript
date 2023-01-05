8f70a356520f4c11239030e13bff17ca19458baf
DeroMal <derrickmla123@gmail.com>
2023-01-05 20:02:04 +0300

    1st

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,34 @@
+interface Student {
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
+let student_1: Student;
+let student_2: Student;
+
+const studentsList: Array<Student> = [student_1, student_2];
+
+const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+const table: HTMLTableElement = document.createElement("table");
+const thead: HTMLTableSectionElement = document.createElement("thead");
+const tbody: HTMLTableSectionElement = document.createElement("tbody");
+const rowHead: HTMLTableRowElement = thead.insertRow(0);
+const cell1Head: HTMLTableCellElement = rowHead.insertCell(0);
+const cell2Head: HTMLTableCellElement = rowHead.insertCell(1);
+
+cell1Head.innerHTML = "firstName";
+cell2Head.innerHTML = "location";
+table.append(thead);
+
+studentsList.forEach((student) => {
+  const row: HTMLTableRowElement = tbody.insertRow(0);
+  const cell1: HTMLTableCellElement = row.insertCell(0);
+  const cell2: HTMLTableCellElement = row.insertCell(1);
+
+  cell1.innerHTML = student.firstName;
+  cell2.innerHTML = student.location;
+});
+
+table.append(tbody);
+body.append(table);

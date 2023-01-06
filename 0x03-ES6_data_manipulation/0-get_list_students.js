ff8b75b1654e9718ef36a7dc0ca30c849fe93367
Derrick L. Mayiku <40387583+DeroMal@users.noreply.github.com>
2023-01-07 00:03:55 +0300

    Update 0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,9 @@
-export default function getListStudents () {
-  return new Array(
-    {
-      id: 1,
-      firstname: 'Guillaume',
-      location: 'San Francisco'
-    },
-    {
-      id: 2,
-      firstname: 'James',
-      location: 'Columbia'
-    },
-    {
-      id: 5,
-      firstname: 'Serena',
-      location: 'San Francisco'
-    }
-  );
+function getListStudents() {
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 }
+
+export default getListStudents;
